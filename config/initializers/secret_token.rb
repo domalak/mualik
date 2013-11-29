95d32ca890e6990e62015cf82bda0e5ce0d7d53c
domalak <domalak@gmail.com>
2013-11-29 23:21:23 +0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mualik::Application.config.secret_key_base = '9ba84d27d821f63b87034234d89e9c28ef41b3be9500c00f5c1536e77bbe155db82c7561899571240309df4898a01aae14b7227416f51036c3c5e79387ac3c4f'
